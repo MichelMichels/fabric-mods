bf821b0e0ba4a506ffc53330a6e7639ada79ff70
Michel Michels <michel@miche.ls>
2024-02-28 08:10:03 +0100

    Initial commit

--- src/first-mod/src/client/java/com/example/ExampleModClient.java
+++ src/first-mod/src/client/java/com/example/ExampleModClient.java
@@ -1,10 +1,12 @@
 package com.example;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 
 public class ExampleModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		BlockRenderLayerMap.INSTANCE.putBlock(ExampleMod.MARKER_BLOCK, RenderLayer.getCutout());
 	}
 }
--- src/first-mod/src/main/java/com/example/ExampleMod.java
+++ src/first-mod/src/main/java/com/example/ExampleMod.java
@@ -2,14 +2,30 @@ package com.example;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.block.Block;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExampleMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+    public static final Logger LOGGER = LoggerFactory.getLogger("first-mod");
+
+	public static final Block MARKER_BLOCK  = new Block(FabricBlockSettings.create().nonOpaque().strength(0f));
+
+	public static final Item CROSS =
+			Registry.register(Registries.ITEM, new Identifier("first-mod", "cross"),
+					new Cross(new FabricItemSettings()));
+
+	public static final Item CHECKMARK =
+			Registry.register(Registries.ITEM, new Identifier("first-mod", "checkmark"),
+					new Checkmark(new FabricItemSettings()));
 
 	@Override
 	public void onInitialize() {
@@ -17,6 +33,9 @@ public class ExampleMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("This is the first mod by Michel Michels");
+
+		Registry.register(Registries.BLOCK, new Identifier("first-mod", "marker_block"), MARKER_BLOCK);
+		Registry.register(Registries.ITEM, new Identifier("first-mod", "marker_block"), new BlockItem(MARKER_BLOCK, new FabricItemSettings()));
 	}
 }
